4e18be76c957da980db22a57b31fe2b28ffcb0c8
Qing Sheng <shengoo@hotmail.com>
2015-05-05 11:06:42 +0800

    add

---
+++ javatest/src/stringtest/formattest/DateFormatTest.java
@@ -0,0 +1,29 @@
+package stringtest.formattest;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class DateFormatTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DateFormatTest test = new DateFormatTest();
+		System.out.println(test.validateDate("2014-04-30 00:00:60"));
+		System.out.println(test.validateDate("2014.04.30 00:00:00"));
+	}
+	
+	/*
+	 * Validate date string with formatter
+	 */
+	public boolean validateDate(String date_string) {
+		try {
+			DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			format.parse(date_string);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+}
