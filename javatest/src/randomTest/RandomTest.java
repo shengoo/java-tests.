cbcd22e7944280bee0571cd64a8a23947ac922ec
Qing Sheng <shengoo@hotmail.com>
2015-05-27 14:57:51 +0800

    add

---
+++ javatest/src/randomTest/RandomTest.java
@@ -0,0 +1,32 @@
+package randomTest;
+
+import java.util.Random;
+
+public class RandomTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random random = new Random(3);
+		System.out.println(random.nextInt());
+		System.out.println(random.nextInt());
+		System.out.println(random.nextInt());
+		System.out.println(random.nextInt());
+		System.out.println(random.nextInt());
+		System.out.println(random.nextInt());
+		System.out.println(random.nextInt());
+		System.out.println(random.nextInt());
+		System.out.println(random.nextInt());
+		System.out.println(random.nextInt());
+		System.out.println(random.nextInt(3));
+		System.out.println(random.nextInt(3));
+		System.out.println(random.nextInt(3));
+		System.out.println(random.nextInt(3));
+		System.out.println(random.nextInt(3));
+		System.out.println(random.nextInt(3));
+		System.out.println(random.nextInt(3));
+		System.out.println(random.nextInt(3));
+		System.out.println(random.nextInt(3));
+		System.out.println(random.nextInt(3));
+	}
+
+}
